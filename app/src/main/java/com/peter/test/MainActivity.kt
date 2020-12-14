0d5b61e5a171372978814c2591b5365bbf02294e
wntjd200@naver.com <wntjd200@naver.com>
2020-12-14 23:23:00 +0900

    seekBar and ProgressBar

--- app/src/main/java/com/peter/test/MainActivity.kt
+++ app/src/main/java/com/peter/test/MainActivity.kt
@@ -1,19 +1,14 @@
 package com.peter.test
 
-import android.content.DialogInterface
 import android.os.Bundle
-import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
+import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import com.peter.test.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
-    var message :String = ""
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -23,37 +18,24 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setListener() {
-        binding.apply {
-            dialogButton.setOnClickListener {
-                showMessage()
+        binding.seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                binding.value.text = "onProgressChanged : $progress"
+                binding.progressBar.progress = progress
             }
-        }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                binding.value.text = "onStartTrackingTouch : ${seekBar!!.progress}"
+                binding.progressBar.progress = seekBar.progress
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                binding.value.text = "onStopTrackingTouch : ${seekBar!!.progress}"
+                binding.progressBar.progress = seekBar.progress
+            }
+
+        })
+
     }
-
-    private fun showMessage(){
-        var builder : AlertDialog.Builder = AlertDialog.Builder(this)
-                .setTitle("안내").setMessage("종료하시겠습니까?").setIcon(android.R.drawable.ic_dialog_alert)
-
-                .setPositiveButton("예", DialogInterface.OnClickListener {
-                    dialog, which ->
-                    message= "예버튼이 눌렸습니다"
-                    Toast.makeText(this,message, Toast.LENGTH_SHORT).show()
-                })
-
-                .setNegativeButton("아니오", DialogInterface.OnClickListener {
-                    dialog, which ->
-                    message= "아니오 버튼이 눌렸습니다"
-                    Toast.makeText(this,message, Toast.LENGTH_SHORT).show()
-
-                })
-                .setNeutralButton("취소", DialogInterface.OnClickListener {
-                    dialog, which ->
-                    message= "취소 버튼이 눌렸습니다"
-                    Toast.makeText(this,message, Toast.LENGTH_SHORT).show()
-                })
-        var dialog : AlertDialog = builder.create()
-        dialog.show()
-    }
-
-
 }
