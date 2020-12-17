f89fc904e175826c24dcd24a96a02a78f55e9b14
wntjd200@naver.com <wntjd200@naver.com>
2020-12-18 00:56:02 +0900

    layoutInflate

--- app/src/main/java/com/peter/test/MainActivity.kt
+++ app/src/main/java/com/peter/test/MainActivity.kt
@@ -1,41 +1,30 @@
 package com.peter.test
 
+import android.content.Context
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.CheckBox
 import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.peter.test.databinding.ActivityMainBinding
+import com.peter.test.databinding.Sub1Binding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
-
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
         super.onCreate(savedInstanceState)
         setListener()
     }
 
     private fun setListener() {
-        binding.seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
-
-            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                binding.value.text = "onProgressChanged : $progress"
-                binding.progressBar.progress = progress
+        binding.apply {
+            button.setOnClickListener {
+                var inflater : LayoutInflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+                inflater.inflate(R.layout.sub1,container,true)
             }
-
-            override fun onStartTrackingTouch(seekBar: SeekBar?) {
-                binding.value.text = "onStartTrackingTouch : ${seekBar!!.progress}"
-                binding.progressBar.progress = seekBar.progress
-            }
-
-            override fun onStopTrackingTouch(seekBar: SeekBar?) {
-                binding.value.text = "onStopTrackingTouch : ${seekBar!!.progress}"
-                binding.progressBar.progress = seekBar.progress
-            }
-
-        })
-
+        }
     }
 }
